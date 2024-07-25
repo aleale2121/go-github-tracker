7f3750d4e458d664da59997ac1e4b5b957616e97
alefew <alefewyimer4@gmail.com>
2024-07-25 18:46:00 +0300

    github api client update

--- commits-monitor-service/internal/pkg/githubrestclient/githubrestclient.go
+++ commits-monitor-service/internal/pkg/githubrestclient/githubrestclient.go
@@ -20,56 +20,6 @@ func NewGithubRestClient(Config *models.Config) GithubRestClient {
 
 const base_url = "https://api.github.com"
 
-func (gp GithubRestClient) FetchRepositories(since string) ([]models.RepositoryResponse, error) {
-	fetchRepoUrl := base_url + fmt.Sprintf("/users/%s/repos?sort=updated&direction=desc", gp.Config.GithubUsername)
-	if since != "" {
-		fetchRepoUrl += fmt.Sprintf("&since=%s", since)
-	}
-	fmt.Println(fetchRepoUrl)
-	request, err := http.NewRequest(http.MethodGet, fetchRepoUrl, nil)
-	request.Header.Add("Accept", "application/vnd.github+json")
-	request.Header.Add("Authorization", fmt.Sprintf("Bearer %s", gp.Config.GithubToken))
-	request.Header.Add("X-GitHub-Api-Version", "2022-11-28")
-
-	if err != nil {
-		log.Println(err)
-		return nil, err
-	}
-
-	response, err := http.DefaultClient.Do(request)
-	if err != nil {
-		log.Println(err)
-		return nil, err
-	}
-	fmt.Println(response.StatusCode)
-	fmt.Println(response.Body)
-	defer response.Body.Close()
-
-	// Read the response body into a string
-	bodyBytes, err := io.ReadAll(response.Body)
-	if err != nil {
-		log.Println("Error reading response body:", err)
-		return nil, err
-	}
-
-	// Print the response body as a string
-	bodyString := string(bodyBytes)
-	fmt.Println("Fetch Response Body: ", bodyString)
-
-	// Optionally, you can print the status code
-	fmt.Println("Fetch Response Status Code: ", response.StatusCode)
-
-	// Parse the JSON response body into your data model
-	var repositories []models.RepositoryResponse
-	err = json.Unmarshal(bodyBytes, &repositories)
-	if err != nil {
-		log.Println("Error unmarshalling response body:", err)
-		return nil, err
-	}
-
-	return repositories, nil
-}
-
 func (gp GithubRestClient) FetchCommits(repositoryName, since string) ([]models.CommitResponse, error) {
 	fetchRepoUrl := base_url + fmt.Sprintf("/repos/%s/%s/commits", gp.Config.GithubUsername, repositoryName)
 	if since != "" {
@@ -90,21 +40,14 @@ func (gp GithubRestClient) FetchCommits(repositoryName, since string) ([]models.
 	}
 	defer response.Body.Close()
 
-	// Read the response body into a string
 	bodyBytes, err := io.ReadAll(response.Body)
 	if err != nil {
 		log.Println("cm Error reading response body:", err)
 		return nil, err
 	}
 
-	// Print the response body as a string
-	// bodyString := string(bodyBytes)
-	// fmt.Println("Response Body:", bodyString)
-
-	// Optionally, you can print the status code
 	fmt.Println("cm Response Status Code:", response.StatusCode)
 
-	// Parse the JSON response body into your data model
 	var repositories []models.CommitResponse
 	err = json.Unmarshal(bodyBytes, &repositories)
 	if err != nil {
